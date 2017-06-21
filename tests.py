5486df53f8362312ae371bf3c87520e68e196c53
miguelbritos91 <miguelbritos91@gmail.com>
2017-06-21 19:59:10 -0300

    nueva aplicación suma

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 import web
+import suma
 
 class FlaskrTestCase(unittest.TestCase):
     def setUp(self):
@@ -10,4 +11,13 @@ class FlaskrTestCase(unittest.TestCase):
         assert b'Hola mundo' in rv.data
 
 if __name__ == '__main__':
+    unittest.main()
+
+
+
+class TestMyModule(unittest.TestCase):
+    
+    def test_sum(self):
+        self.assertEqual(mymodule.sum(5, 7), 12)
+if __name__ == "__main__":
     unittest.main()
--- web.py
+++ web.py
@@ -1,6 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
+import suma
+
 @app.route('/')
 def index():
-  return 'Hola mundo'
+  return 'Hola mundo - '
+  return sum(3,5)
